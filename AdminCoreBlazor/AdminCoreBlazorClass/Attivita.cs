429ace735e5cddd938e57a20690a8c652623e8de
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-09-29 10:29:37 +0200

    agg 5

--- AdminCoreBlazor/AdminCoreBlazorClass/Attivita.cs
+++ AdminCoreBlazor/AdminCoreBlazorClass/Attivita.cs
@@ -9,22 +9,11 @@ namespace AdminCoreBlazorClass
         public int RifTipoAttivita { get; set; }
         public string Dettagli { get; set; } = string.Empty;
         public decimal Costo { get; set; }
-}
 
-    public class PeriodoAttivita
-    {
-        public int ID { get; set; }
-        public int Mese { get; set; }
-        public int Anno { get; set; }
-        public string Descrizione { get; set; } = string.Empty;
-    }
+        public int Tipo_Id { get; set; }
+        public virtual Tipo? Tipo { get; set; }
 
-    public class TipologiaAttivita
-    {
-        public int ID { get; set; }
-        public string TipoAttivita { get; set; } = string.Empty;
-        public string ColoreHTML { get; set; } = string.Empty;
-        public string Tipologia { get; set; } = string.Empty;
-        public string ColoreAzione { get; set; } = string.Empty;
+        public Guid Perido_Id { get; set; }
+        public virtual Periodo? Periodo { get; set; }
     }
 }
--- AdminCoreBlazor/AdminCoreBlazorShared/Entity.cs
+++ AdminCoreBlazor/AdminCoreBlazorShared/Entity.cs
@@ -8,6 +8,16 @@
     public abstract class Entity<TKey> : IEntity<TKey>
     {
         public TKey Id { get; set; } = default!;
+    }
 
+
+    public interface IArchiveableEntity<TKey> : IEntity<TKey>
+    {
+        bool IsDeleted { get; }
+    }
+
+    public abstract class ArchiveableEntity<TKey> : Entity<TKey>, IArchiveableEntity<TKey>
+    {
+        public bool IsDeleted { get; set; }
     }
 }
