e52280fe03c357074af7b1b5199f0c4cbdb12870
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-10-29 22:21:08 +0100

    agg

--- BlazorWebApp/obj/Debug/net8.0/BlazorWebApp.AssemblyInfo.cs
+++ BlazorWebApp/obj/Debug/net8.0/BlazorWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53301dd680f594f5457f7204c0ac474435bc286b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c88d60d72da225a1d624e06fae3a5dc544465f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
