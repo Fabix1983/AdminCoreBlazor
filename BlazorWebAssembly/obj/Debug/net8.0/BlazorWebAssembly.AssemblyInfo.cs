6da3abbb4f5f923f636bab47e3c10a39e09f5590
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-11-04 22:17:47 +0100

    agg Trend

--- BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
+++ BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0d586ea271e5cc44a798099f6aefd3827d2412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50da886f01d589e67527d75f64ec0e61cba98121")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
