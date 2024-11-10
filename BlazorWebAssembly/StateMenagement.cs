c444484578d11f4ab6bc73ae89101b9bd60b47e9
Fabio Gianlorenzo <gianlorenzof@libero.it>
2024-11-10 08:41:33 +0100

    agg

---
+++ BlazorWebAssembly/StateMenagement.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorWebAssembly
+{
+    public class StateMenagement
+    {
+        public string utente { get; set; } = "";
+
+        public string api_url = "http://api.admincoreblazor.it";
+
+        public string JsonNormalized(string json)
+        {
+            json = json.ToString().Replace("\\u0022", "\"");
+            json = json.ToString().Replace("\"{", "{");
+            json = json.ToString().Replace("}\"", "}");
+
+            return json;
+        }
+    }
+}
--- BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
+++ BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866882709f6fb0f33505ac9605f42c85ee42e273")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
