154079bf59a66c1747367867bf0354beb81172cc
Fabio Gianlorenzo <gianlorenzof@libero.it>
2025-02-09 10:55:47 +0100

    agg Prev

--- BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
+++ BlazorWebAssembly/obj/Debug/net8.0/BlazorWebAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b00d318fa0adfcaa29915e4a2dd64b88452d23e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec27605b517953a4c2ca3e5e4210824f37ce270")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
